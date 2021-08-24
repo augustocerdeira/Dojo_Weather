7830f170a89e9ea35eb476ded87097f12ae883eb
Augusto Cerdeira <augusto.borbi@gmail.com>
2021-08-23 17:34:43 -0700

    Liking_Likes

---
+++ JS/Liking_Likes/script.js
@@ -0,0 +1,23 @@
+var count =1;
+var countLikes = document.querySelector("#count")
+
+function add() {
+    count++;
+    countLikes.innerText = count + " like(s)"
+}
+
+var count1 =1;
+var countLikes1 = document.querySelector("#count1")
+
+function add1() {
+    count1++;
+    countLikes1.innerText = count1 + " like(s)"
+}
+
+var count2 =1;
+var countLikes2 = document.querySelector("#count2")
+
+function add2() {
+    count2++;
+    countLikes2.innerText = count2 + " like(s)"
+}
